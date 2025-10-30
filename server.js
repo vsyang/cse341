6428e6ab078266b207c50fb1c9c91b1f6440c293
vsyang <yang.vanessa85@gmail.com>
2025-10-30 17:01:06 -0600

    connection to rest keeps being rejected

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
 
-const port = process.env.PORT || 8080;
 const app = express();
+const port = process.env.PORT || 8080;
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  })
+    })
   .use('/professional', professionalRoutes);
 
 mongodb.initDb((err, mongodb) => {
