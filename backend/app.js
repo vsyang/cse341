d011a37e8a27a8e8264d1342ef3ff6c5ac4d36f2
vsyang <yang.vanessa85@gmail.com>
2025-10-30 17:28:57 -0600

    moved folders and files into appropriate "backend" folder

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const cors = require('cors');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
 
-const app = express();
 const port = process.env.PORT || 8080;
+const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use((req, res, next) => {
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-    })
+  })
   .use('/professional', professionalRoutes);
 
 mongodb.initDb((err, mongodb) => {
